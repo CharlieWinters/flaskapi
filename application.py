c58cd00ab52f0f3112c97945502424d0b9280f0c
Sean Winters <sean.m.winters@gmail.com>
2016-01-20 07:40:27 -0800

    left app in two more places changed over now.

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ application = Flask(__name__)
 #test
 application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///people.db'
 application.config['DEBUG'] = True
-db = SQLAlchemy(app)
+db = SQLAlchemy(application)
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,7 +22,7 @@ class Datapoints(db.Model):
 
 db.create_all()
 
-api_manager = APIManager(app, flask_sqlalchemy_db=db)
+api_manager = APIManager(application, flask_sqlalchemy_db=db)
 api_manager.create_api(Person, methods=['GET', 'POST', 'DELETE', 'PUT'])
 api_manager.create_api(Datapoints, methods=['GET', 'POST', 'DELETE', 'PUT'])
 
